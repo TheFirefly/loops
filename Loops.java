11d1172a458ad82ec7ef45587775bc8d275dcb69
Sasha Ouellet <spaouellet@me.com>
2013-09-03 08:06:51 -0600

    Equivalent for loop

--- Loops.java
+++ Loops.java
@@ -2,10 +2,14 @@ public class Loops {
 	
 	public static void main(String[] args) {
 		
-		int x = 1;
-		while (x <= 100) {
-			System.out.println(x);
-			x++;
+		// int x = 1;
+		// while (x <= 100) {
+		// 	System.out.println(x);
+		// 	x++;
+		// }
+
+		for (int i = 1 ; i <= 100 ; i++) {
+			System.out.println(i);
 		}
 
 
